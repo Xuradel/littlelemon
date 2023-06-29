0bd9f80ca602ede0e51a1e91311368b48b04a040
Xuradel <leagueofxuradel@gmail.com>
2023-06-29 10:33:14 -0600

    added about section

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { ReactComponent as ReactLogo } from '../images/Logo.svg'
+
+let hamburger = document.querySelector('.hamburger');
+let navLinks = document.getElementById('nav-links');
+let links = document.querySelectorAll('.links');
+
+// hamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('hide');
+//     hamburger.classList.toggle('lines-rotate');
+// })
+
+// for (let i = 0; i < links.length; i++) {
+//     links[i].addEventListener('click', () => {
+//         navLinks.classList.toggle('hide');
+//     })
+// }
+
 function Nav() {
     return (
-        <nav>
+        <nav className='nav-container'>
             <div className='logo'>
-                <ReactLogo></ReactLogo>
+                <ReactLogo className='logo-img'></ReactLogo>
             </div>
-            <ul>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Menu</a></li>
-                <li><a href='#'>Reservations</a></li>
-                <li><a href='#'>Order Online</a></li>
-                <li><a href='#'>Login</a></li>
+            <div className='hamburger'>
+                <span className='lines'></span>
+                <span className='lines'></span>
+                <span className='lines'></span>
+            </div>
+            <ul id='nav-links'>
+                <li><a href='#' className='links'>Home</a></li>
+                <li><a href='#' className='links'>About</a></li>
+                <li><a href='#' className='links'>Menu</a></li>
+                <li><a href='#' className='links'>Reservations</a></li>
+                <li><a href='#' className='links'>Order Online</a></li>
+                <li><a href='#' className='links'>Login</a></li>
             </ul>
         </nav>
     )
